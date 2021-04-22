9a98b458fcdcc44b4e6cca31201241cd71eee81a
mbb1997 <mbranch1997@gmail.com>
2021-04-22 16:44:19 -0700

    added files

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,91 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Linq.Enumerable;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Material mat;
+    public TextAsset level;
+    float width = 1;
+    float height = 1;
+
+    [System.Serializable] //put this in front of each class
+    public class Shape  // define the idea of a shape
+    {
+        public int id;
+        public int x;
+        public int y;
+        public int w;
+        public int h;
+        public string color;
+        public int[] touches;
+
+    }
+
+    [System.Serializable]
+    public class ShapeList // define the idea of a list of shapes
+    {
+        public Shape[] shapes;
+    }
+
+
+    public ShapeList shapelist = new ShapeList();  // create an instance to work with
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shapelist = JsonUtility.FromJson<ShapeList>(level.text); // run this on th instance to read the json file in
+        Mesh[] meshList = new Mesh[shapelist.shapes.Length];
+
+
+        for (int i = 0; i < shapelist.shapes.Length; i++)
+        {
+            Debug.Log("sdgfsdfhgdf");
+            //Console.WriteLine(i);
+            Mesh mesh = new Mesh();
+
+            Vector3[] vertices = new Vector3[4]
+            {
+            new Vector3(0, 0, 0),
+            new Vector3(width, 0, 0),
+            new Vector3(0, height, 0),
+            new Vector3(width, height, 0)
+            };
+            mesh.vertices = vertices;
+
+            int[] tris = new int[6]
+            {
+            // lower left triangle
+            0, 2, 1,
+            // upper right triangle
+            2, 3, 1
+            };
+            mesh.triangles = tris;
+            // mesh.name = "1234";
+
+            MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+            // meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+
+            MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+
+
+            meshFilter.mesh = mesh;
+            mesh.RecalculateNormals();
+            meshList[i] = mesh;
+        }
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/MeshGen.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGen : MonoBehaviour
+{
+    public float width = 1;
+    public float height = 1;
+
+    public void Start()
+    {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+                Debug.Log("Mouse Down Hit the following object: " + hit.collider.name);
+            else
+                Debug.Log("Nothing was hit!");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[4]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(width, 0, 0),
+            new Vector3(0, height, 0),
+            new Vector3(width, height, 0)
+        };
+        mesh.vertices = vertices;
+
+        int[] tris = new int[6]
+        {
+            // lower left triangle
+            0, 2, 1,
+            // upper right triangle
+            2, 3, 1
+        };
+        mesh.triangles = tris;
+        mesh.name = "1234";
+
+        /*
+        Vector3[] normals = new Vector3[4]
+        {
+            -Vector3.forward,
+            -Vector3.forward,
+            -Vector3.forward,
+            -Vector3.forward
+        };
+        mesh.normals = normals;
+
+        Vector2[] uv = new Vector2[4]
+        {
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(0, 1),
+            new Vector2(1, 1)
+        };
+        mesh.uv = uv;
+        */
+
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+       // meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+
+
+        meshFilter.mesh = mesh;
+        mesh.RecalculateNormals();
+       
+    }
+
+
+
+
+}
